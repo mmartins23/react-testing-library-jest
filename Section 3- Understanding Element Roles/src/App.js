f411997616f5114ddb83085597c5ce5daec85c57
Marlon Martins <marlon-martins@outlook.com>
2024-06-24 14:47:58 +0200

    Section 3. Understanding Element Roles

---
+++ Section 3- Understanding Element Roles/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user])
+  }
+
+  return (
+    <div className="App">
+      <UserForm onUserAdd={onUserAdd} />
+      <hr/>
+      <UserList users={users}/>
+    </div>
+  );
+}
+export default App;
